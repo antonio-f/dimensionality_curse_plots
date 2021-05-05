564f2d68f0c75045fc19fbf26a6dc857aa70c6fa
Antonio <26195457+antonio-f@users.noreply.github.com>
2021-05-05 12:56:05 +0200

    Update curse_plots.py

--- curse_plots.py
+++ curse_plots.py
@@ -3,14 +3,14 @@ from mpl_toolkits.mplot3d import Axes3D
 import numpy as np 
 
 # 1D
-x = np.random.rand(40,1)
+x = np.random.rand(40, 1)
 y = np.ones(len(x))
-plt.scatter(x , y)
+plt.scatter(x, y)
 plt.xlabel('1D points')
 plt.show()
 
 # 2D
-points = np.random.rand(40,2)
+points = np.random.rand(40, 2)
 plt.scatter(points[:,0], points[:,1])
 plt.xlabel('2D points')
 plt.show()
@@ -18,6 +18,6 @@ plt.show()
 # 3D
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-points = np.random.rand(40,3)
+points = np.random.rand(40, 3)
 ax.scatter(points[:,0], points[:,1], points[:,2])
 plt.show()
